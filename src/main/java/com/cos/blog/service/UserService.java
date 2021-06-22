0bab79bc5c1ad6e16c39eac938a2d4b26e3e318f
hyeongwoo26 <hyeongwoo26@gmail.com>
2021-06-22 23:39:14 +0900

    51강 XSS와 CSRF

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -1,7 +1,9 @@
 package com.cos.blog.service;
 
 
+import com.cos.blog.model.RoleType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.cos.blog.model.User;
@@ -15,8 +17,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
     @Transactional
     public void 회원가입(User user) {
+        String rawPassword = user.getPassword(); //1234원문
+        String encPassword = encoder.encode(rawPassword); //해쉬
+        user.setPassword(encPassword);
+        user.setRole(RoleType.USER);
         userRepository.save(user);
     }
 
